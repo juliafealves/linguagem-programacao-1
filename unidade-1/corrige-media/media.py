2dafa25c2728377bb774a3fb55ba0aa7f6cc9007
Júlia Fernandes Alves <juliafealves@gmail.com>
2017-10-23 10:50:42 -0300

    Exercício corrige média.

---
+++ unidade-1/corrige-media/media.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+# Corrige Média
+# (C) 2017 Júlia Alves / UFCG Programação 1
+
+nota1 = 10
+nota2 = 5
+
+media = (nota1 + nota2) / 2.0
+
+print 'Nota 1: %4.1f' % float(nota1)
+print 'Nota 2: %4.1f' % float(nota2)
+print 'Média : %4.1f' % media
