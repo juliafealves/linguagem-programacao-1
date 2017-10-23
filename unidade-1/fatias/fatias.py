159a3cb297b48457cc2086a64303afaebb0ce1c0
Júlia Fernandes Alves <juliafealves@gmail.com>
2017-10-23 10:42:05 -0300

    Exercícios Hello World e Nome 3 vezes

--- unidade-1/fatias/fatias.py
+++ unidade-1/fatias/fatias.py
@@ -2,10 +2,10 @@
 # Número de Fatias
 # (C) 2017, Júlia Alves / UFCG Programação 1
 
-convidados = float(raw_input())
-opcaoInteira = 32 / int(convidados)
+convidados = int(raw_input())
+opcaoInteira = 32 / convidados
 opcaoInteiraResto = 32 % convidados
-opcaoFloat = 32 / convidados
+opcaoFloat = 32 / float(convidados)
 
 print 'Opção 1: %d fatias cada, %d de resto' % (opcaoInteira, opcaoInteiraResto)
 print 'Opção 2: %.2f fatias cada' % (opcaoFloat)
