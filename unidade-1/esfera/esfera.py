9581568e48a36c418f1ccdf41b9667ab958385e7
juliafealves <juliafealves@gmail.com>
2017-10-23 00:50:05 -0300

    Exercício volume de uma Esfera.

---
+++ unidade-1/esfera/esfera.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+# Volume de uma Esfera
+# (C) 2017 Júlia Alves / UFCG Programação 1
+
+import math
+
+raio = float(raw_input())
+volume = (4 * math.pi * raio ** 3) / 3
+
+print '%.2f' % volume
