4ae391c7196af683bf1e7c2cbdc6289744198855
juliafealves <juliafealves@gmail.com>
2017-10-29 13:38:39 -0300

    Exercício Média Final.

---
+++ unidade-1/media-final/media_final.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+# Média Final
+# (C) 2017, Júlia Alves / UFCG Programação 1
+
+# Entradas das médias
+media_parcial = float(raw_input())
+prova_final = float(raw_input())
+
+# Calculando a média final
+media_final = (media_parcial * 0.6) + (prova_final * 0.4)
+
+# Imprimindo a média final
+print "Média final: %.1f" % media_final
