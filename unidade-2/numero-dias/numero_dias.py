48d612fda1893e2417a1f060d814f4bd89795696
juliafealves <juliafealves@gmail.com>
2017-10-29 22:37:56 -0300

    Exercício Número de Dias.

---
+++ unidade-2/numero-dias/numero_dias.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+# Número de Dias
+# (C) 2017, Júlia Alves / UFCG Programação 1
+
+# Entrada dos dados
+segundos = int(raw_input())
+
+# Calculando os dias
+dias = segundos / (3600 * 24)
+
+# Imprimindo
+print dias
