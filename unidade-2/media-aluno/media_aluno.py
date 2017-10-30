2a8e6584f7b9f37301f2cc37f67e79bc523bd551
juliafealves <juliafealves@gmail.com>
2017-10-29 22:45:29 -0300

    Exercício Média do Aluno.

---
+++ unidade-2/media-aluno/media_aluno.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+# Média do Aluno
+# (C) 2017, Júlia Alves / UFCG Programação 1
+
+peso1 = 2.0
+peso2 = 3.0
+peso3 = 5.0
+
+# Entrada dos dados referente as notas.
+nota1 = float(raw_input())
+nota2 = float(raw_input())
+nota3 = float(raw_input())
+
+# Calculando a média
+media = (nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / 10
+
+# Imprimindo a média do aluno.
+print media
