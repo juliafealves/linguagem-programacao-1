f220bc6c1d66ede537ebc021dbadb650ca1eaa28
juliafealves <juliafealves@gmail.com>
2017-10-30 02:19:47 -0300

    Exercício Parte Fracionária.

---
+++ unidade-2/parte-fracionaria/parte_fracionaria.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+# Parte Fracionária
+# (C) 2017, Júlia Alves / UFCG Programação 1
+
+# Entrada dos dados
+numero = float(raw_input())
+fracionaria = numero - int(numero)
+
+# Imprimindo a parte fracionária.
+print fracionaria
