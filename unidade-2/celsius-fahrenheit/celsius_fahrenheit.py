e44a77db491efe08aacb57f0205c944515c4b57c
juliafealves <juliafealves@gmail.com>
2017-10-29 22:28:44 -0300

    Exercício Celsius Fahrenheit.

---
+++ unidade-2/celsius-fahrenheit/celsius_fahrenheit.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+# Celsius Fahrenheit
+# (C) 2017, Júlia Alves / UFCG Programação 1
+
+# Entrada de dados referente a temperatura em Celsius.
+celsius = float(raw_input())
+
+# Calculando Fahrenheit para Celsius.
+fahrenheit = (9.0 / 5.0) * celsius + 32
+
+# Imprimindo a temperatura.
+print "%.1f" % fahrenheit
