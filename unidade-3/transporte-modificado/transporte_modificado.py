ff2a9ce0e847c875479435cd250928655185babb
juliafealves <giT#8985>
2017-11-08 21:37:46 -0300

    Exercício: Transporte Modificado.

--- unidade-3/transporte-modificado/transporte_modificado.py
+++ unidade-3/transporte-modificado/transporte_modificado.py
@@ -2,19 +2,23 @@
 # Aluno: Júlia Alves
 # Matricula: 117211383
 # Atividade: Transporte Modificado - Unidade 3
+import math
 
-preco_tav = 100
-preco_onibus = 22
-preco_taxi_passageiro = 50
-preco_taxi_total = 200
+preco_tav = 100.00
+preco_onibus = 22.00
+preco_taxi = 200.00
 
+# Entrada dos dados relacionados a quantidade de passageiros e dinheiro disponível para passagens.
 passageiros = int(raw_input())
 dinheiro = float(raw_input())
 
-gasto_tav = passageiros *  preco_tav
-gasto_taxi = (passageiros / 4.0) * preco_taxi_total
+# Calculando os possíveis gastos com transportes.
+gasto_tav = passageiros * preco_tav
+quantidade_taxis = math.ceil(passageiros / 4.0)
+gasto_taxi = quantidade_taxis * preco_taxi
 gasto_onibus = passageiros * preco_onibus
 
+# Verifica as opções de transporte em ordem de menor tempo de viagem.
 if gasto_tav <= dinheiro:
     print "TAV. R$ %.2f" % gasto_tav
 elif gasto_taxi <= dinheiro:
