8591ccb303f145ca1ef279bc58932746e636ac84
juliafealves <giT#8985>
2017-11-16 22:30:10 -0300

    Exercício: Série (8.8).

---
+++ unidade-4/serie88/serie88.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+# Programação 1 - 2017.2 UFCG
+# Aluna: Júlia Fernandes Alves (117211383)
+# Atividade: Série (8.8) - Unidade 4
+
+serie = 8.6
+
+for indice in range(457):
+    serie += 0.2
+    print serie
